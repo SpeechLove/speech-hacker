37f5f3d8f554561bd527dc747b55175962d591cc
Apprentice <apprentice@devbootcamp.com>
2012-11-26 15:08:03 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeechHacker::Application.config.secret_token = '2d8e5d4f0e2989b43eebeadc3fee3b672df50d14563aacc942837889dc39da289d69dc201e1f08b0b66761d076d63f7381bdea3020de1ceb5fd80c2004c0a5e2'
