75b766a42c24ead0c5469390c002095dc2ba9914
Young Cha + Brent Coughenour <apprentice+yc+bc@devbootcamp.com>
2012-11-26 16:31:45 -0800

    deleting old roles and creating new roles through rolify

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,6 @@
 class Role < ActiveRecord::Base
-  # attr_accessible :title, :body
+  has_and_belongs_to_many :users, :join_table => :users_roles
+  belongs_to :resource, :polymorphic => true
+  
+  scopify
 end
