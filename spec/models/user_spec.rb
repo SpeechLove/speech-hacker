e7bd9acd4d0faba7b42be37c92f010527f980543
Young Cha + Brent Coughenour <apprentice+yc+bc@devbootcamp.com>
2012-11-27 12:23:32 -0800

    Add #admin? test for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  context "when user is valid" do
-    let(:user)  { User.new({:name => "Jim", :email => "a@a.com", :password => "blahblah"}) }
+  let (:user) { Fabricate(:user) }
 
+  context "when user is valid" do
     it "should be valid" do
       user.should be_valid
     end
 
     it "has a name" do
-      user.name.should eq "Jim"
+      user.name.should eq user.name
     end
   end
 
   context "when user is invalid" do
-    let(:user) { User.new({:name => "Jim", :email => "a@a.com", :password => "blahblah"})}
-
     it "should return an error for having an invalid name" do
       user.name = ""
       user.should_not be_valid
@@ -31,4 +29,17 @@ describe User do
       user.should_not be_valid
     end
   end
+
+  describe "#admin?" do
+    let (:user_admin) { Fabricate(:user_admin) }
+
+    it "returns true when user is an admin" do
+      user_admin.admin?.should be_true
+    end
+
+    it "returns false when a user is not an admin" do
+      user.admin?.should be_false
+    end
+  end
+
 end
