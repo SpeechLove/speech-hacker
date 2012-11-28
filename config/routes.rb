3639ea3b58e17a993095cf15e76e81a294fec8d2
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-11-28 15:44:25 -0800

    Change order of routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ SpeechHacker::Application.routes.draw do
   root :to => "meetings#index"
   resources :meetings
   resources :attendances, :only => [:create, :update]
-  resources :users
   devise_for :users
+  resources :users
+
   post '/users/:id/make_admin' => 'users#make_admin', :as => 'make_admin_user'
 end
