cd6d3111ae0a890883e9ac1569f58fbe50056558
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-11-27 18:04:27 -0800

    Add super admin role.

--- app/controllers/speeches_controller.rb
+++ app/controllers/speeches_controller.rb
@@ -1,2 +1,5 @@
 class SpeechesController < ApplicationController
+  
+  
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+
   before_filter :authenticate_user!
   before_filter :admin_only, :only => [:index]
 
@@ -10,9 +12,17 @@ class UsersController < ApplicationController
 
   end
 
+  def update
+    authorize! :assign_roles, @user if assigning_roles?
+  end
+
   private
 
   def admin_only
     redirect_to root_path, notice: "You are not authorized to access this page." unless current_user.admin?
+  end
+
+  def assigning_roles?
+    params[:user].has_key?(:roles)
   end
 end
