cdb9a2f15a7e840113d0f239fa353b72d3363e64
Young Cha + Brent Coughenour <apprentice+yc+bc@devbootcamp.com>
2012-11-27 16:01:03 -0800

    added form for attendance creation

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,19 @@
 class AttendancesController < ApplicationController
+	def create
+		logger.info("-------------- attendance create -------------")
+		logger.info(params)
+		if (params[:attend] == false) 
+			user_meeting_role_id = MeetingRole.find_by_title("Absentee").id
+		else
+			if params[:attendance][:meeting_role_id]
+				user_meeting_role_id = params[:attendance][:meeting_role_id].to_i
+			else
+			  user_meeting_role_id = MeetingRole.find_by_title("Attendee").id
+			end
+		end
+		attendance_params = params[:attendance]
+		attendance_params[:user_id] = current_user.id
+		attendance_params[:meeting_role_id] = user_meeting_role_id
+		@attendance = Attendance.create(attendance_params)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SpeechHacker::Application.routes.draw do
   root :to => "meetings#index"
   resources :meetings
+  resources :attendances, :only => [:create]
   devise_for :users
   get '/users' => 'users#index', :as => 'users'
+
 end
