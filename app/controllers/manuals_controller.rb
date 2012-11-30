2e44e7aa3cb6ddef3cee359c4b550b417b1c39cb
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-11-30 12:18:44 -0800

    fixed bug in manuals controller tests. added pending tests.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,25 +1,22 @@
 class ManualsController < ApplicationController
 
-	def index
-		@manuals = Manual.all
-		@speeches = Hash.new
-		@speeches_test = current_user.speeches
-		logger.info('--------current user--------------')
-		logger.info(current_user.inspect)
-		logger.info('------------database speeches---------')
-		logger.info(Speech.all)
-		@manuals.each do |m|
-			@speeches[m] = current_user.speeches.for_manual(m)
-		end
-		logger.info('------------speeches hash----------')
-		logger.info(@speeches.inspect)
+  #need to add authorization here so that only admins and the records' user can view this page
 
-	end
+  def index
+    @manuals = Manual.all
+    @speeches = Hash.new
+    user = User.find(params[:user_id])
+    #@speeches_test = current_user.speeches
+    #current_user # => User.find(session[:user_id])
+    @manuals.each do |manual|
+      @speeches[manual] = user.speeches.for_manual(manual)
+    end
+  end
 
-	# def show
-	# 	@manual = Manual.find(params[:id])
-	# 	@speeches = current_user.speeches.for_manual(@manual) # this accesses the class method in Speech that queries the database.
-	# end
+  # def show
+  #   @manual = Manual.find(params[:id])
+  #   @speeches = current_user.speeches.for_manual(@manual) # this accesses the class method in Speech that queries the database.
+  # end
 
 
 
