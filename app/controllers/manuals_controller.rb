30ac6c79cfcd4b9c8580c21c34f1d45cd1f60d93
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-11-29 18:30:17 -0800

    added accordion styling to manuals index

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,7 +1,14 @@
 class ManualsController < ApplicationController
 
 	def index
+		#@manual = Manual.find(params[:id])
 		@manuals = Manual.all
+		#@speeches = current_user.speeches.for_manual(@manual) # this accesses the class method in Speech that queries the database.
+		@speeches = Hash.new
+		@manuals.each do |m|
+			@speeches[m] = current_user.speeches.for_manual(m)
+		end
+
 	end
 
 	def show
