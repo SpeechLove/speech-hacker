c6060f87b1b441c3af4a48ffd2db3443a89948c7
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-12-04 12:57:02 -0800

    updated unit tests for manuals model

--- spec/models/manual_spec.rb
+++ spec/models/manual_spec.rb
@@ -4,12 +4,4 @@ describe Manual do
   Fabricate(:manual)
   it { should have_many :projects }
   it { should validate_presence_of :name}
-
-  describe "completed_projects" do
-  	it "should return a list of completed projects" do
-
-  	end
-  end
-
-
 end
