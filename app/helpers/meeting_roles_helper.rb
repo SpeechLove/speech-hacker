f6f882fb39e177a90256892b15a7089734a65e92
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-12-06 11:51:35 -0800

    Revert "Add icons to meetings and meeting roles page. Make sign up link on navbar bold."
    This reverts commit bfeb3e93d801a9dda6783f9db11fb7fcba20c685.

--- app/helpers/meeting_roles_helper.rb
+++ app/helpers/meeting_roles_helper.rb
@@ -1,13 +1,13 @@
 module MeetingRolesHelper
   def edit_meeting_role_link(meeting_role)
     unless meeting_role.required_role?
-      link_to "<i class=\"icon-pencil\"></i> Edit".html_safe, edit_meeting_role_path(meeting_role), :remote => true
+      link_to "Edit", edit_meeting_role_path(meeting_role), :remote => true
     end
   end
 
   def delete_meeting_role_link(meeting_role)
     unless meeting_role.required_role?
-      link_to "<i class=\"icon-remove\"></i> Destroy".html_safe, meeting_role_path(meeting_role), :method => :delete, :remote => true,
+      link_to "Destroy", meeting_role_path(meeting_role), :method => :delete, :remote => true,
                          :confirm => "Are you sure you want to delete this role?"
     end
   end
