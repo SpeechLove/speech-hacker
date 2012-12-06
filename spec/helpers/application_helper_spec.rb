2e9fd305134d6094d518c7db9a385c25c31ed633
Julie Mao + Kerrie Yee <apprentice+jm+ky@devbootcamp.com>
2012-12-06 12:33:53 -0800

    added application_helpers tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,19 +1,54 @@
-# require 'spec_helper'
-#
-# describe ApplicationHelper do
-#   let(:user) { Fabricate(:user) }
-#   let(:meeting) { Fabricate(:meeting) }
-#
-#   describe '#current_project' do
-#     context "when there is a user speech" do
-#
-#     end
-#
-#     context "when there is not a user speech" do
-#       it "returns an empty string" do
-#         helper.stub(:current_user).and_return(user)
-#         helper.current_project(meeting).should eq ""
-#       end
-#     end
-#   end
-# end
+require 'spec_helper'
+
+describe ApplicationHelper do
+  let(:user) { Fabricate(:user) }
+  let(:meeting) { Fabricate(:meeting) }
+  let(:manual) { Fabricate(:manual) }
+  let(:project) { Fabricate(:project) }
+  let(:speech) { Fabricate(:speech) }
+
+  before(:each) do
+    sign_in user
+  end
+
+  describe '#current_speech' do
+    context "when there is a user speech" do
+      it "returns the speech" do
+        helper.stub(:current_user).and_return(user)
+        user.stub(:meeting_speech).and_return(speech)
+        helper.current_speech(meeting).should eq speech
+      end
+    end
+
+    context "when there is no user speech" do
+      it "builds a new speech" do
+        helper.stub(:current_user).and_return(user)
+        new_speech = helper.current_speech(meeting)
+        user.speeches[0].should eq new_speech
+      end
+    end
+  end
+
+  describe '#current_manual' do
+    context "the speech belongs to a project" do
+      it "returns the manual of the current speech" do
+        helper.stub(:current_speech).and_return(speech)
+        speech.stub(:project).and_return(project)
+        helper.current_manual(meeting).should eq project.manual
+      end
+    end
+
+    context "the speech does not belong to a project" do
+      it "returns the first manual" do
+        helper.current_manual(meeting).should eq Manual.first
+      end
+    end
+  end
+
+  describe '#project_select' do
+    it "returns all of the projects of the current speech's manual" do
+      Project.stub(:find_all_by_manual_id).and_return(manual.projects)
+      helper.project_select(meeting).should eq manual.projects
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ Spork.prefork do
 
   RSpec.configure do |config|
     config.include Devise::TestHelpers, :type => :controller
+    config.include Devise::TestHelpers, :type => :helper
     # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
